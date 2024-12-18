1700e12c290485c4f047192330ba7310cc0ca957
prerna <rajputprerna1503@gmail.com>
2024-12-18 19:16:08 +0530

    js

---
+++ script.js
@@ -0,0 +1,118 @@
+console.log("hello");
+console.log("hello");
+console.log("hello");
+console.log("hellooooooooooo");
+
+//npm install --save package-name // for eaxitng folder
+//npm install -g pacakage name// for global
+//npm install -D pacakage name// for dev dependency
+
+//for running nodemon = nodemon filename
+//for exit nodemon = control C
+
+// js is loosely coupled language
+//word vs keyword
+//statement vs expression
+//statement - multiple line + single output a = 2,b = 4, a+b=c
+//expression - single line + single output c = 5 + 6
+//case sensitivity
+
+//number system...............
+// 0 1 2 3 4 5 6 7 8 9 = decimal number
+// 0 1 - binary
+
+//ascii code = ............................
+
+//A B C
+//! # ^ &
+//a -97
+//A - 65
+//1 - 49
+//" " - 32
+
+// output method...................
+// console.log();
+
+
+// comments.........................
+// single line comments
+// this is a singke line comment
+
+
+// multiple line comments
+// this
+// is 
+// a multiline
+// comment
+
+
+// variables...........
+// variable nothing but a container which is used to store values..
+// var varname = "value";
+
+// var varname -- declaration
+// var varname = "value" -- intialization
+// var varname = "10" -- reintialization/updation
+
+     
+// scope............................
+
+{
+  // var a = "2";      global scopic variable
+  // let b = "5";    local scopic variable
+  // const c = "34"  local scopic constant variable
+
+}
+
+
+
+// datatypes....................
+// number - 12 ,-12 ,.12 ,-12.12
+// string = "" , ''
+// Boolean = true/false
+// null - null(Object)
+// undefined - undefined
+// Symbol - homework
+
+//naming convention for variables......................
+//1. relatablem | age = 23
+//2. starts with lower/upper case,_
+//3. never start with number or special character other than \
+//4. must be single word
+//5. in case of multiple words we use sperators/patterns like:
+
+   //1. camelCasePattern = childAge = 23
+  //2. snake_case_pattern = parent_age = 34
+  //3. pascalCasePattren = GrandParentAge = 45
+  //4. kebab-case-pattern = child-age = 42 (not allowed)
+
+
+  // oprators................................................
+  // 1. arithematic operator = +,-,*,/,( % modulo),(++ increment, --decremrnt)
+  // 2.assignment/shorthand operators =, +=, -=, *=, /=, %=
+  // let a = 4
+  // let b = 6
+  // a = a + b
+  // a += b
+  
+
+  // let a = 45;
+  // let b = 10;
+  // let b = ++a; pre increament
+  // let b = a++; post increament
+
+  // console.log(a);
+  // console.log(b);
+
+//  let res = ++a + --b + a-- - b++ + a + --a - b-- + a++ - ++b;
+//  let res = 46 + 9 + 46 - 9 + 45 + 44 - 10 + 44 - 10;
+// console.log(res); 205
+// console.log(a); 45
+// console.log(b); 10
+
+  
+  
+
+  //3. comparison oprators ==,===, !=, !==, >, <, >=, <=
+  // 4. logical oprators &&, ||, !
+
